a4e6eb2dc9a842ea41eab05bb2943d29eda37a3a
Salah eddine berret <146879159+SBerret3301@users.noreply.github.com>
2023-10-13 15:36:29 +0100

    Add files via upload

---
+++ 10-0.1.py
@@ -0,0 +1,12 @@
+x = int(input("enter a number : "))
+n = 0
+while n < 10 :
+    n = n + 1
+    print(x + n)
+#-----------------------------------------------
+n = 0
+while True :
+    n = n + 1
+    print(x + n)
+    if n > 9 :
+        break
